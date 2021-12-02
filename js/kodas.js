e3575dbca6300abaf6f055c8767c8a6e4a833c51
Tautvydas Dulskis <tautvydas.dulskis@hostinger.com>
2021-12-02 21:19:47 +0200

    JS reziu paieska

--- js/kodas.js
+++ js/kodas.js
@@ -1,8 +1,30 @@
-let a = 1;
-let tekstas = 'Kintamojo a reikšmė: ';
+let a = 6;
+let b = 6;
 
-console.log(tekstas + a);
+let c = -9;
+let d = 415
 
-a = 2;
+let x = '???';
+let y = '???';
 
-console.log(tekstas + a);
+if (a > b || d < c) {
+    window.alert('Sankitra negalima');
+}
+
+if (c <= a && a <= d) {
+    x = a;
+} else if (a <= c && c <= b) {
+    x = c;
+}
+
+if (a <= d && d <= b){
+    y = d;
+} else if (c <= b && b <= d){
+    y = b;
+}
+
+let atsakymas = 'X: ' + x + ' Y: ' + y;
+
+if (atsakymas !== 'X: ??? Y: ???') {
+    document.getElementById('ats').innerHTML = atsakymas;
+}
