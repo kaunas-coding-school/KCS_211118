6670e0b9c3239549ccf688ac404de06dec9287ed
Tautvydas Dulskis <tautvydas.dulskis@hostinger.com>
2021-12-08 21:34:11 +0200

    jquery

--- js/kodas.js
+++ js/kodas.js
@@ -1,30 +1,9 @@
-let a = 6;
-let b = 6;
-
-let c = -9;
-let d = 415
-
-let x = '???';
-let y = '???';
-
-if (a > b || d < c) {
-    window.alert('Sankitra negalima');
-}
-
-if (c <= a && a <= d) {
-    x = a;
-} else if (a <= c && c <= b) {
-    x = c;
-}
-
-if (a <= d && d <= b){
-    y = d;
-} else if (c <= b && b <= d){
-    y = b;
-}
-
-let atsakymas = 'X: ' + x + ' Y: ' + y;
-
-if (atsakymas !== 'X: ??? Y: ???') {
-    document.getElementById('ats').innerHTML = atsakymas;
-}
+$('*').click(
+    function (e) {
+        let tagName = e.target.nodeName;
+        let tags = $(tagName);
+        let tagCount = tags.length;
+        $('#kasas').text('Paspaudete ant ' + tagName + ' tago. Tokiu elementu yra: ' + tagCount)
+        tags.css('background-color', 'yellow');
+    }
+);
