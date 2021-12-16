1c1941ad047aef0a04bff54c8fec30957fad7046
Tautvydas Dulskis <tautvydas.dulskis@hostinger.com>
2021-12-16 09:19:13 +0200

    Darbas su JSON ir REGEXP

--- index.php
+++ index.php
@@ -15,3 +15,18 @@ spausdintiFaila($resursas, $kiek);
 copy($manoFailas, 'libs/mano_failas_new.html');
 
 unlink($manoFailas);
+
+echo "Darbas su JSON:<br>";
+$jsonString = '{"Tautvydas": 2000, "Jonas": 3000}';
+echo $jsonString;
+$array = json_decode($jsonString, true);
+$array = array_merge($array, ['kestas' => 4000]);
+foreach ($array as $key => $item) {
+    echo $key.' = '.$item.'<br>';
+}
+echo json_encode($array);
+
+echo "Darbas su REGEXP:<br>";
+$text = "We at Guru strive to make quality to the masses";
+$text = preg_replace("/Guru/", '<span style="background:yellow">Guru</span>', $text);
+echo $text;
